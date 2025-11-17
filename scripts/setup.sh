b98bedc2a00dbf38ce7053cd6f7da0551752c325
Trifon <trifonnt@users.noreply.github.com>
2025-11-17 21:13:48 +0200

    Upgrade JHipster generator to version 8.11.0

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -53,7 +53,7 @@ su -c "yarn config set prefix /home/vagrant/.yarn-global" vagrant
 npm install -g yo
 
 # install JHipster
-npm install -g generator-jhipster@8.4.0
+npm install -g generator-jhipster@8.11.0
 
 # install JHipster UML
 npm install -g jhipster-uml@2.0.3
